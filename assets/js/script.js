d1455528735bc786f120c3baa7f3186b801bb8c5
Micah Pellum <pellummicah@gmail.com>
2020-11-18 21:44:37 -0700

    form fields

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,10 @@ var formEL = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
  var createTaskHandler = function(event) {
+
   event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']");
+  console.log(taskNameInput);
 
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
